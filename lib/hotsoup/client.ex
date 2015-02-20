c285a69555b454d4259afc0a0dadd440b92722e3
Nicolas Michel <nicolas.michel.lava@gmail.com>
2015-02-20 23:07:37 +0100

    Change from Client.match/2 to Client.match/3 with matching on server state.

--- lib/hotsoup/client.ex
+++ lib/hotsoup/client.ex
@@ -25,48 +25,29 @@ defmodule Hotsoup.Router.Client do
       
       defoverridable [init: 1, nomatch: 2]
 
-      import unquote(__MODULE__), only: [match: 2]
+      import unquote(__MODULE__), only: [match: 3]
 
-      @matchers []
       @before_compile unquote(__MODULE__)
     end
   end
   
   defmacro __before_compile__(_env) do
-    quote unquote: false do
-      @matchers_by_exp @matchers |> Enum.group_by(fn({k, _}) ->
-                                                      k
-                                                  end)
-                                 |> Enum.map(fn {k, v} ->
-                                                  fv = v |> Enum.map(fn {k, e} -> e end)
-                                                  {k, fv}
-                                             end)
-
-     @matchers_by_exp
-     |> Enum.each fn({expr, matchers}) ->
-                      def do_match({:node, unquote(expr), node}, state) do
-                        state = unquote(matchers)
-                                |> Enum.reduce(state, fn({m, f}, state) ->
-                                                          Kernel.apply(m, f, [node, state])
-                                                      end)
-                        {:noreply, state}
-                      end
-                  end
-
-     def do_match(msg, state) do
-       nomatch(msg, state)
-     end
+    quote do			
+			def do_match(msg, state) do
+				nomatch(msg, state)
+			end
     end
   end
 
-  defmacro match(expr, [do: code]) do
-    << _, _, t :: binary>> = "#{:random.uniform}"
-    fname = :"#{__CALLER__.module}.#{expr}.#{t}"
+  defmacro match(expr, state, [do: code]) do
+		generate(expr, state, code)
+  end
+
+	defp generate(expr, state, code) do
     quote do
-      def unquote(fname)(var!(jnode), var!(state)) do
+      def do_match(var!(jnode), unquote(state)) do
         unquote(code)
       end
-      @matchers [{unquote(expr), {unquote(__CALLER__.module), unquote(fname)}} | @matchers]
     end
-  end
+	end
 end

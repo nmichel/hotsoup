6217db497ead8c81f9377e2b7d292724691d2058
Nicolas Michel <nicolas.michel.lava@gmail.com>
2015-03-02 13:12:02 +0000

    Move node matching DSL to Hotsoup.Router.Facade

---
+++ lib/hotsoup/facade.ex
@@ -0,0 +1,107 @@
+defmodule Hotsoup.Router.Facade do
+  defmacro __using__(_opts) do
+    quote do
+      def nomatch(state, jnode) do
+        {:stop, {:nomatch, jnode}, state}
+      end
+
+      defoverridable [nomatch: 2]
+
+      import unquote(__MODULE__)
+
+      @matchers []
+      @before_compile unquote(__MODULE__)
+    end
+  end
+  
+  defmacro __before_compile__(_env) do
+    quote unquote: false do
+      @matchers_by_exp @matchers |> Enum.group_by(fn({k, _expr, _state, _conds, _code}) ->
+                                                      k
+                                                  end)
+                                 |> Enum.map(fn({k, v = [{_k, expr, state, _cond_0, _code_0}|_]}) ->
+                                                 {{expr, state}, Enum.map(v, fn({_k, _expr_n, _state_n, cond_n, code_n}) ->
+                                                                                 {cond_n, code_n}
+                                                                             end)}
+                                             end)
+
+      @matchers_by_exp
+      |> Enum.each fn({{expr, state}, conds_code}) ->
+                       bindings = expr |> extract_capture_names |> build_bindings
+                       body = build_body(conds_code)
+                       svar = Macro.var(:svar, nil)
+                       def do_match(unquote(expr), svar = unquote(state), var!(jnode)) do
+                         unquote_splicing(bindings)
+                         unquote(body)
+                       end
+                   end
+
+      def do_match(pattern, state, jnode) do
+        nomatch(state, jnode)
+      end
+    end
+  end
+
+  defmacro match(expr, state, code) do
+    quote do
+      match(unquote(expr), unquote(state), [], unquote(code))
+    end
+  end
+
+  defmacro match(expr, state, conds, [do: code]) do
+    quote bind_quoted: [key: expr <> Macro.to_string(state),
+                        expr: Macro.escape(expr),
+                        state: Macro.escape(state),
+                        conds: Macro.escape(conds),
+                        code: Macro.escape(code)] do
+      @matchers [{key, expr, state, conds, code} | @matchers]
+    end
+  end
+
+  def extract_capture_names(expr) do
+    Regex.compile("\\(\\?<([A-Za-z0-9_]+)>[^\\)]*\\)")
+    |> elem(1)
+    |> Regex.scan(expr, [capture: :all_but_first])
+    |> List.flatten
+  end
+
+  def build_bindings(vars) do
+    vars
+    |> Enum.map(fn(name) ->
+                    va = String.to_atom(name)
+                    v = Macro.var(va, nil)
+                    quote do
+                      {:ok, l} = Keyword.fetch(elem(var!(jnode), 1), unquote(va))
+                      unquote(v) = l
+                    end
+                end)
+  end
+
+  def build_body([]) do
+    quote bind_quoted: [state: Macro.var(:state, nil),
+                        jnode: Macro.var(:jnode, nil)] do
+      nomatch(state, jnode)
+    end
+  end
+  def build_body([{conds, code} | tail]) do
+    ifcond = build_if_conds(conds)
+    elsebody = build_body(tail)
+    quote do
+      if unquote(ifcond) do
+        unquote(code)
+      else
+        unquote(elsebody)
+      end
+    end
+  end
+
+  def build_if_conds(conds) do
+    conds
+    |> Enum.reduce(true,
+                   fn({:when, c}, acc) ->
+                       quote do
+                         unquote(c) and unquote(acc)
+                       end
+                   end)
+  end
+end
---
+++ test/hotsoup/facade_test.exs
@@ -0,0 +1,86 @@
+defmodule Hotsoup.Router.FacadeTest.MyClient do
+  use Hotsoup.Router.Facade
+
+  match "\"foo\"", %{case: :case1} do
+    :case1
+  end
+
+  match "42", %{case: :case1} do
+    :case2
+  end
+
+  match "\"foo\"", %{case: :case2} do
+    :case3
+  end
+
+  match "42", %{case: :case2} do
+    :case4
+  end
+
+  match "[*, (?<val>_)]", _state,
+    when: List.first(val) == 42
+  do
+    :case5
+  end
+
+  match "42", _state do
+    :case6
+  end
+  
+  match "[*, (?<v1>_), {_: (?<v2>)}]", _state,
+    when: check_some_properties(v2),
+    when: is_list(v1),
+    when: List.first(Enum.reverse(v1)) == 42
+  do
+    :case7
+  end
+
+  match "[*, (?<v1>_), {_: (?<v2>)}]", _state,
+    when: check_some_properties(v2)
+  do
+    :case8
+  end
+  
+  defp check_some_properties(l) when is_list(l) do
+    l == ["foo", :foo]
+  end
+  defp check_some_properties(l) do
+    false
+  end
+end
+
+defmodule Hotsoup.Router.FacadeTest do
+  use ExUnit.Case
+  alias Hotsoup.Router.FacadeTest.MyClient
+
+  test "match first on expression, then on data", _context do
+    assert :case2 == MyClient.do_match("42", :mock, %{case: :case1})
+    assert :case4 == MyClient.do_match("42", :mock, %{case: :case2})
+    assert :case6 == MyClient.do_match("42", :mock, :whatelse)
+    assert :case1 == MyClient.do_match("\"foo\"", :mock, %{case: :case1})
+    assert :case3 == MyClient.do_match("\"foo\"", :mock, %{case: :case2})
+  end
+
+  test "match when: conditions" do
+    assert :case5 == MyClient.do_match("[*, (?<val>_)]", {:mock_with_captures, [val: [42, :foo]]}, %{case: :case1})
+    assert :case8 == MyClient.do_match("[*, (?<v1>_), {_: (?<v2>)}]", {:mock_with_captures, [v2: ["foo", :foo], v1: :no_match_data]}, :no_match_data)
+    assert :case7 == MyClient.do_match("[*, (?<v1>_), {_: (?<v2>)}]", {:mock_with_captures, [v1: [:bar, 42], v2: ["foo", :foo]]}, :no_match_data)
+  end
+
+  test "return :nomatch when no clause matches" do
+    {:nomatch, _, _} = MyClient.do_match("\"foo\"", :mock, :no_match_data)
+  end
+
+  test "return :nomatch when no :when conditions set matches" do
+    {:nomatch, _, _} = MyClient.do_match("[*, (?<val>_)]", {:mock_with_captures, [val: [:foo, 42]]}, %{case: :case1})
+  end
+
+  test "error caused when variables present in expressions are not bound" do
+    {:badmatch, _} = catch_error MyClient.do_match("[*, (?<val>_)]", {:mock_with_captures, [not_val: [42]]}, :no_match_data)
+    {:badmatch, _} = catch_error MyClient.do_match("[*, (?<v1>_), {_: (?<v2>)}]", {:mock_with_captures, [v1: [:bar, 42]]}, :no_match_data)
+  end
+
+  test "error caused when variables present in expressions are not bound event if all conditions are met" do
+    {:badmatch, _} = catch_error MyClient.do_match("[*, (?<v1>_), {_: (?<v2>)}]", {:mock_with_captures, [v2: ["foo", :foo]]}, :no_match_data)
+  end
+end

59b990c5f6173e7445254a684bdaf7f4389b2214
Nicolas Michel <nicolas.michel.lava@gmail.com>
2015-03-05 15:11:56 +0000

    Add a bigger test "pongistes.ex"

---
+++ test/hotsoup/pongistes.exs
@@ -0,0 +1,130 @@
+defmodule Ping do
+  use Hotsoup.Client.GenServer
+
+  def init(nil) do
+    {:ok, rid} = Hotsoup.Cluster.get_router(ttl: 1000)
+    super {0, rid}
+  end
+
+  match "{\"action\": \"ping\"}", {counter, rid} do
+    Hotsoup.Router.route(rid, :jsx.decode("{'action': 'pong'}"))
+    {:noreply, {counter+1, rid}}
+  end
+
+  match "{\"action\": \"stop\"}", state do
+    {:stop, state}
+  end
+end
+
+defmodule Pong do
+    use Hotsoup.Client.GenServer
+
+    def start_link(n) when is_integer(n) do
+      super n
+    end
+    
+    def init(n) do
+      {:ok, rid} = Hotsoup.Cluster.get_router(ttl: 1000)
+      super {0, {n, rid}}
+    end
+
+    match "{\"action\": \"pong\"}", state = {n, {n, rid}} do
+      Hotsoup.Router.route(rid, :jsx.decode("{'action': 'stop'}"))
+      {:noreply, state}
+    end
+    match "{\"action\": \"pong\"}", {counter, inner = {_, rid}} do
+      Hotsoup.Router.route(rid, :jsx.decode("{'action': 'ping'}"))
+      {:noreply, {counter+1, inner}}
+    end
+
+    match "{\"action\": \"stop\"}", state do
+      {:stop, state}
+    end
+end
+
+defmodule Starter do
+    use Hotsoup.Client.GenServer
+
+    def init(nil) do
+      {:ok, rid} = Hotsoup.Cluster.get_router(ttl: 1000)
+      super rid
+    end
+
+    match "{\"action\": \"start\"}", rid do
+      Hotsoup.Router.route(rid, :jsx.decode("{'action': 'ping'}"))
+      {:stop, rid}
+    end
+end
+
+defmodule Monitor do
+  use Hotsoup.Client.GenServer
+    
+  def init(nil) do
+    super %{ping: 0, pong: 0, other: 0, total: 0}
+  end
+
+  match "{\"action\": (?<action>_)}", state,
+    when: action == ["ping"]
+  do
+    {:noreply, %{state | ping: state[:ping]+1, total: state[:total]+1}}
+  end
+
+  match "{\"action\": (?<action>_)}", state,
+    when: action == ["pong"]
+  do
+    {:noreply, %{state | pong: state[:pong]+1, total: state[:total]+1}}
+  end
+  # clause
+  #   match "{\"action\": (?<action>_)}", state = %{other: other} do ...
+  # won't work as expected, because clauses
+  #   match "{\"action\": (?<action>_)}", state, ...
+  # will catch all nodes routed through  "{\"action\": (?<action>_)}"
+  # Use a default clause and destructure inside it, as follows.
+  #
+  # Warning ! The following clause will get all nodes except "ping" and "pong", even "stop" might be caught.
+  # 
+  # match "{\"action\": (?<action>_)}", state do
+  #   IO.puts ["action: action ! ", inspect(jnode)]
+  #   %{other: other, total: total} = state
+  #   {:noreply, %{state | other: other+1, total: total+1}}
+  # end
+  match "{\"action\": (?<action>_)}", state,
+    when: action != ["stop"]
+  do
+    %{other: other, total: total} = state
+    {:noreply, %{state | other: other+1, total: total+1}}
+  end
+  match "{\"action\": (?<action>_)}", state do
+    # noop clause
+    {:noreply, state}
+  end
+
+  # Warning ! The following clause is a "catchall". Each node will match the expression
+  # Therefore, "start", "ping", "pong" message will get caught by this clause (and the
+  # others too);  even "stop" depending on if this clause is called  before the one
+  # dedicated to "stop" (see bellow).
+  # 
+  match "{\"action\": (?<action_too>_)}", state do
+    {:noreply, state}
+  end
+
+  match "{\"action\": \"stop\"}", state do
+    {:stop, %{state | other: state[:other]+1, total: state[:total]+1}}
+  end
+end
+
+Process.flag(:trap_exit, true)
+
+Pong.start_link 10
+Ping.start_link
+Starter.start_link
+{:ok, monitor} = Monitor.start_link
+
+{:ok, rid} = Hotsoup.Cluster.get_router(ttl: 1000)
+Hotsoup.Router.route(rid, :jsx.decode("{'action': 'start'}"))
+
+receive do
+  {:EXIT, ^monitor, s} ->
+    IO.puts ["******** => ", inspect(s)]
+end
+
